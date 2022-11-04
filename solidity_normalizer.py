ee66117f1f7b63c3ce82601fc5b913143186ceb5
smartcontract-detect-yzu <71329338+smartcontract-detect-yzu@users.noreply.github.com>
2022-11-04 16:53:19 +0800

    源代码normalize

---
+++ solidity_normalizer.py
@@ -0,0 +1,156 @@
+
+import os
+import re
+
+
+        
+    
+class SrcNormalize:
+    def __init__(self, target_dir, sol_file) -> None:
+        self.sol_file = target_dir + sol_file
+        self.norm_dir = target_dir + "normalized_src//"
+        self.norm_sol_file = target_dir + "normalized_src//" + sol_file
+        if not os.path.exists(self.norm_dir):
+            os.mkdir(self.norm_dir)
+        
+        self.lines = open(self.sol_file).readlines()
+        self.SafeLowLevelCallMap = ['safeTransferFrom', 'safeTransfer', "sendValue","functionCallWithValue",  "functionCall", "functionStaticCall"]
+        self.TodCallMap = [".safeApprove", ".approve", ".safeIncreaseAllowance", ".safeDecreaseAllowance"]
+
+    def do_normalize_file(self):
+
+        with open(self.norm_sol_file, "w+") as n_f:
+            for line in self.lines:
+                delete_flag = 0
+                if ".add" in line:
+                    result = re.search(r'\.add\((.*?)\)',line) # .sub(匹配)
+                    if result is None:
+                        # rewardPerTokenStored.add(
+                        #     lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(derivedSupply)
+                        # );
+                        pass
+
+                    else:
+                        _params = result.group(1)  # amount, "SafeERC20: decreased allowance below zero"
+                        _first_sep = str(_params).find(',') # 第一个 ","
+                        if -1 != _first_sep:
+                            key = _params[_first_sep:]
+                            op_new = ""
+                            line = line.replace(key, op_new)
+
+                    op_new = " {} ".format("+")
+                    key = ".{}".format("add")
+                    line = line.replace(key, op_new)
+
+                if ".sub" in line:
+                    result = re.search(r'\.sub\((.*?)\)',line) # .sub(匹配)
+                    if result is None:
+                        pass
+                    else:
+                        _params = result.group(1)  # amount, "SafeERC20: decreased allowance below zero"
+                        _first_sep = str(_params).find(',') # 第一个 ","
+                        if -1 != _first_sep:
+                            key = _params[_first_sep:]
+                            op_new = ""
+                            line = line.replace(key, op_new)
+
+                    op_new = " {} ".format("-")
+                    key = ".{}".format("sub")
+                    line = line.replace(key, op_new)
+
+                if ".mul" in line:
+                    result = re.search(r'\.mul\((.*?)\)',line) # .sub(匹配)
+                    if result is None:
+                        pass
+                    else:
+                        _params = result.group(1)  # amount, "SafeERC20: decreased allowance below zero"
+                        _first_sep = str(_params).find(',') # 第一个 ","
+                        if -1 != _first_sep:
+                            key = _params[_first_sep:]
+                            op_new = ""
+                            line = line.replace(key, op_new)
+
+                    op_new = " {} ".format("*")
+                    key = ".{}".format("mul")
+                    line = line.replace(key, op_new)
+
+                if ".div" in line:
+                    result = re.search(r'\.div\((.*?)\)',line) # .sub(匹配)
+                    if result is None:
+                        pass
+                    else:
+                        _params = result.group(1)  # amount, "SafeERC20: decreased allowance below zero"
+                        _first_sep = str(_params).find(',') # 第一个 ","
+                        if -1 != _first_sep:
+                            key = _params[_first_sep:]
+                            op_new = ""
+                            line = line.replace(key, op_new)
+
+                    op_new = " {} ".format("/")
+                    key = ".{}".format("div")
+                    line = line.replace(key, op_new)
+
+                if  "function" in line and "nonReentrant" in line:
+                    line = line.replace("nonReentrant", "")
+                
+                if  "function" in line and "onlyOwner" in line:
+                    line = line.replace("onlyOwner", "")
+                
+                for llc in self.SafeLowLevelCallMap:
+                    if llc in line:
+                        line = line.replace(llc, "call")
+                
+                for tod in self.TodCallMap:
+                    if tod in line:
+                        if tod != ".approve": 
+                            line = line.replace(tod, ".approve")
+                        else:
+                            if "0" in line:
+                                delete_flag = 1
+
+                if "toUint" in line:
+                    result = re.search(r'\.toUint(.*?)\(\)',line)
+                    if result is not None:
+                        _cast = "uint{}".format(result.group(1))
+                        pos = result.span()[0] - 1
+
+                        # xxx var.toUint(xxx) 由.向前找第一个不是字符的位置
+                        while pos > 0:
+                            if (not line[pos].isalpha()) and (line[pos] not in [".", "[", "]"]):
+                                pos += 1
+                                break
+                            pos -= 1
+
+                        var_name = line[pos:result.span()[0]]
+                        org = "{}{}".format(var_name, result.group(0))
+                        new = "{}({})".format(_cast, var_name)
+                        line = line.replace(org, new)
+                
+                if "toInt" in line:
+                    result = re.search(r'\.toInt(.*?)\(\)',line)
+                    if result is not None:
+                        _cast = "int{}".format(result.group(1))
+                        pos = result.span()[0] - 1
+
+                        # xxx var.toUint(xxx) 由.向前找第一个不是字符的位置
+                        while pos > 0:
+                            if (not line[pos].isalpha()) and (line[pos] not in [".", "[", "]"]):
+                                pos += 1
+                                break
+                            pos -= 1
+                        var_name = line[pos:result.span()[0]]
+                        org = "{}{}".format(var_name, result.group(0))
+                        new = "{}({})".format(_cast, var_name)
+                        line = line.replace(org, new)
+                        
+
+                if 0 == delete_flag:
+                    n_f.write(line)
+
+
+if __name__ == '__main__':
+    
+    # norlamize_function_with_safemath(EXAMPLE_PERFIX + '0x06a566e7812413bc66215b48d6f26321ddf653a9/' + "Gauge.sol")
+    nom = SrcNormalize("example//" + '0x00f401c1e60C9eBf48b1c22c0D87250Cc54F979f//', "VirtualBalance.sol")
+    nom.do_normalize()
+    
